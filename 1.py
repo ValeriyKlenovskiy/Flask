6fdf8c13e18752be1fb299e4a7a7673d8693b822
Valeriy Klenovskiy <Vklenovskii@gmail.com>
2022-06-09 15:21:44 +0300

    after 2

--- 1.py
+++ 1.py
@@ -1,13 +1,14 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-
+menu = ['Установка', 'Первое приложение','Обратная связь']
 @app.route('/')
+@app.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', menu=menu)
 @app.route('/about')
 def about():
-    return '<h1>about</h1>'
+    return render_template('about.html', title='О сайте', menu=menu)
 
 if __name__ == '__main__':
     app.run(debug=True)
