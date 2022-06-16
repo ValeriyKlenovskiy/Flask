0fe6f8ca5594117ddfcbc7b4dc683b52849ceef7
Valeriy Klenovskiy <Vklenovskii@gmail.com>
2022-06-16 16:31:44 +0300

    10

--- FDataBase.py
+++ FDataBase.py
@@ -1,6 +1,8 @@
 import math
 import sqlite3
 import time
+import re
+from flask import url_for
 
 
 class FDataBase:
@@ -17,18 +19,29 @@ class FDataBase:
         except:
             print('Ошибка чтения из БД')
         return []
-    def addPost(self, title, text):
+    def addPost(self, title, text, url):
         try:
+            self.__cur.execute(f"SELECT COUNT() as `count` FROM posts WHERE url LIKE '{url}'")
+            res = self.__cur.fetchone()
+            if res['count'] > 0:
+                print("Статья с таким url уже существует")
+                return False
+            # base = url_for('static', filename='images_html')
+            #
+            # text = re.sub(r"(?P<tag><img\s+[^>]*src=)(?P<quote>[\"'])(?P<url>.+?)(?P=quote)>",
+            #               "\\g<tag>" + base + "/\\g<url>>",
+            #               res['text'])
+
             tm = math.floor(time.time())
-            self.__cur.execute("INSERT INTO posts VALUES(NULL, ?, ?, ?)", (title, text, tm))
+            self.__cur.execute("INSERT INTO posts VALUES(NULL, ?, ?, ?, ?)", (title, text, url, tm))
             self.__db.commit()
         except sqlite3.Error as e:
             print("Ошибка добавления статьи в БД"+str(e))
             return False
         return True
-    def getPost(self, id_post):
+    def getPost(self, alias):
         try:
-            self.__cur.execute(f"SELECT title, text FROM posts WHERE id = {id_post}")
+            self.__cur.execute(f"SELECT title, text FROM posts WHERE url LIKE '{alias}'")
             res = self.__cur.fetchone()
             if res:
                 return res
@@ -37,7 +50,7 @@ class FDataBase:
 
     def getPostsAnnounce(self):
         try:
-            self.__cur.execute(f"SELECT id, title, text FROM posts ORDER BY time DESC")
+            self.__cur.execute(f"SELECT id, title, text, url FROM posts ORDER BY time DESC")
             res = self.__cur.fetchall()
             if res:
                 return res
